014885fb7ca58b41528c09beb337562f829e4c64
rishi003 <rishabh.awatani@gmail.com>
2020-07-14 23:53:27 +0400

    Get page html prototype working

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 // this is the starting point for the application.
 const express = require("express");
 const app = express();
+const puppeteer = require("puppeteer");
 const port = 3000;
+
+async function getPage(pageName) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(pageName, { waitUntil: "networkidle0" });
+  const data = await page.evaluate(() => {
+    return document.querySelector("*").outerHTML;
+  });
+  return data;
+}
 
 app.use(express.static("public"));
 
 app.get("/formData", (req, res) => {
-  console.log(req.query.searchTerm);
+  getPage(req.query.searchTerm).then((val) => {
+    res.type("text/plain");
+    res.send(val);
+  });
 });
 
 app.listen(port, () => {
