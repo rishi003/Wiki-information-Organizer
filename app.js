eabb5bb0cda6e97f9e2db7b4afd455b6749e11f5
rishi003 <rishabh.awatani@gmail.com>
2020-07-16 00:08:18 +0400

    Page Evaluation and form submission without redirect

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ const app = express();
 const puppeteer = require("puppeteer");
 const port = 3000;
 
-async function getPage(pageName) {
+async function getWikiPage(pageName) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/" + pageName, {
+    waitUntil: "networkidle0",
+  });
+  const imgs = await page.$$eval("img", (imgs) =>
+    imgs.map((img) => img.getAttribute("src"))
+  );
+  console.log(imgs);
+  const data = await page.evaluate(() => {
+    return document.querySelector("*").outerHTML;
+  });
+  return data;
+}
+
+async function getWikiLink(hrefAttr) {
+  return "https://en.wikipedia.org/" + hrefAttr;
+}
+
+async function getPage(pageName) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setRequestInterception(true);
+
+  page.on("request", (req) => {
+    if (req.resourceType() == "stylesheet" || req.resourceType() == "font") {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
   await page.goto(pageName, { waitUntil: "networkidle0" });
   const data = await page.evaluate(() => {
     return document.querySelector("*").outerHTML;
@@ -17,8 +49,7 @@ async function getPage(pageName) {
 app.use(express.static("public"));
 
 app.get("/formData", (req, res) => {
-  getPage(req.query.searchTerm).then((val) => {
-    res.type("text/plain");
+  getWikiPage(req.query.searchTerm).then((val) => {
     res.send(val);
   });
 });
